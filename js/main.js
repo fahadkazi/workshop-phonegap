cba2debacc5278404cc48fedef777a88d95ddd27
fahadkz <fhkz2005@gmail.com>
2015-07-11 17:27:22 +0530

    added a few more files

--- js/main.js
+++ js/main.js
@@ -1,24 +1,56 @@
 var app = {
 
-    findByName: function() {
-        console.log('findByName');
-        this.store.findByName($('.search-key').val(), function(employees) {
-            var l = employees.length;
-            var e;
-            $('.employee-list').empty();
-            for (var i=0; i<l; i++) {
-                e = employees[i];
-                console.log(e);
-                $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + ' ' + e.title + '</a></li>');
-            }
-        });
-    },
+    
+
+    // findByName: function() {
+    //     var self = this;
+    //     this.store.findByName($('.search-key').val(), function(employees) {
+    //         $('.employee-list').html(self.employeeLiTpl(employees));
+    //     });
+    // },
 
     initialize: function() {
         MemoryStore = WebSqlStore;
-        this.store = new MemoryStore();
-        $('.search-key').on('keyup', $.proxy(this.findByName, this));
-    }
+        this.store = new MemoryStore(function() {
+            $('body').html(new HomeView(self.store).render().el);
+        });
+        this.registerEvents();
+    },
+
+    registerEvents: function() {
+        var self = this;
+        // Check of browser supports touch events...
+        if (document.documentElement.hasOwnProperty('ontouchstart')) {
+            // ... if yes: register touch event listener to change the "selected" state of the item
+            $('body').on('touchstart', 'a', function(event) {
+                $(event.target).addClass('tappable-active');
+            });
+            $('body').on('touchend', 'a', function(event) {
+                $(event.target).removeClass('tappable-active');
+            });
+        } else {
+            // ... if not: register mouse events instead
+            $('body').on('mousedown', 'a', function(event) {
+                $(event.target).addClass('tappable-active');
+            });
+            $('body').on('mouseup', 'a', function(event) {
+                $(event.target).removeClass('tappable-active');
+            });
+        }
+    },
+
+    // renderHomeView: function() {
+    //     $('body').html(this.homeTpl());
+    //     //$('.search-key').on('keyup', $.proxy(this.findByName, this));
+    // },
+
+    // showAlert: function (message, title) {
+    //     if (navigator.notification) {
+    //         navigator.notification.alert(message, null, title, 'OK');
+    //     } else {
+    //         alert(title ? (title + ": " + message) : message);
+    //     }
+    // },
 
 };
 
